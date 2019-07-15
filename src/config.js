ceffa4365f3c59fb4af413484e883c1e053ec55e
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-07-15 18:03:03 +0530

    Update server url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const server = "http://localhost:8080";
+// const server = "http://localhost:8080";
+const server = "http://35.231.254.47:8080";
 const client = "gf#0BP36u0l3fdMU2.JIdM94xwpbdwzU4ZQe6a3AN";
 
 export { client, server };
